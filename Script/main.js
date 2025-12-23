f286fa298f6c9a45d5ccfff2f7bbd27c751b50db
Masum Prodhan <masumbillahprodhan003@gmail.com>
2025-12-23 14:27:12 +0600

    second commit

--- Script/main.js
+++ Script/main.js
@@ -1,1 +1,23 @@
 let table = new DataTable('#myTable');
+
+
+document.getElementById("form").addEventListener('submit',function(e){
+    e.preventDefault();
+
+    let name = document.getElementById('exampleInputName').value.trim();
+    let email = document.getElementById('exampleInputEmail').value.trim();
+   
+    if(name !== ''){
+        
+        if(email !==''){
+            window.alert("Submission Succesful")
+        }
+        else{
+             document.getElementById("email-error").innerHTML = "<P class='text-danger'> Please enter your email </p>"
+    }
+        
+    }
+    else{
+        document.getElementById("name-error").innerHTML = "<P class='text-danger'> Please enter your name </p>"
+    }
+})
